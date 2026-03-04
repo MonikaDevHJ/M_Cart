76e4b5294e098575dac4cd0ff7055389d522ce53
Monika H J <monikahj7@gmail.com>
2026-03-04 12:02:57 +0530

    gift item has been done

---
+++ app/gifts/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GiftPage = () => {
+  return (
+    <div>
+      <p className='text-center justify-center'>GiftPages</p>
+    </div>
+  )
+}
+
+export default GiftPage
