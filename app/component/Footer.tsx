c625614c68d643c0f36411e7c73c41d8dc0300a3
Monika H J <monikahj7@gmail.com>
2026-03-09 15:16:09 +0530

    Macrt

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -1,42 +1,45 @@
 const Footer = () => {
-    return (
-        <div className="mt-10">
-            <div className="bg-gray-900  rounded-2xl">
+  return (
+    <div className="mt-10">
+      <div className="bg-gray-900  rounded-2xl">
+        <div className="flex px-10 py-15 gap-36">
+          <div className="">
+            <p className="text-gray-300 font-bold"> About</p>
 
+            <div className="mt-5">
+              <p className="text-white">Contact Us</p>
+              <p className="text-white">About Us</p>
+              <p className="text-white">Carrear</p>
 
-
-                <div className="flex px-10 py-15 gap-40">
-                    <div className="">
-                        <p className="text-white font-bold"> About</p>
-                    </div>
-                    <div className="">
-                        <p className="text-white font-bold">Group Companies</p>
-                    </div>
-
-                    <div className="">
-                        <p className="text-white font-bold">Help</p>
-                    </div>
-
-                    <div className="">
-                        <p className="text-white font-bold">Consumer Policy</p>
-                    </div>
-
-                    <div className="">
-                        <p className="text-white font-bold">Mail Us</p>
-
-                    </div>
-
-                    <div className="">
-                        <p className="text-white font-bold">Register Offfice Addresses</p>
-
-                    </div>
-                </div>
-
-
-
+              <p className="text-white">Press</p>
+              <p className="text-white">Corparate Information</p>
             </div>
+          </div>
+          <div className="">
+            <p className="text-gray-300   font-bold">Group Companies</p>
+          </div>
+
+          <div className="">
+            <p className="text-gray-300 font-bold">Help</p>
+          </div>
+
+          <div className="">
+            <p className="text-gray-300 font-bold">Consumer Policy</p>
+          </div>
+
+          <div className="">
+            <p className="text-gray-300 font-bold">Mail Us</p>
+          </div>
+
+          <div className="">
+            <p className="text-gray-300 font-bold">
+              Register Offfice Addresses
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
