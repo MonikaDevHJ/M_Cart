c326346313eeb87b07b3b3eae28e1dcf853a7d0b
Monika H J <monikahj7@gmail.com>
2026-03-30 12:31:07 +0530

    Seller dashbord

--- app/component/seller/dashboard/SellerRecentOrder.tsx
+++ app/component/seller/dashboard/SellerRecentOrder.tsx
@@ -1,19 +1,52 @@
 const SellerRecentOrder = () => {
   const Order = [
     {
-      id: "#01",
+      id: "#1021  ",
       Customer: "John Doe",
       status: "Delivered",
       Amount: "₹1,200",
       Date: "Apr 20, 2025"
     },
     {
-
+      id: "#1022",
+      Customer: "Emma Smith",
+      status: "Pending",
+      Amount: "₹850",
+      Date: "Apr 18, 2024"
+    },
+    {
+      id: "#1021",
+      Customer: "Michael Lee",
+      status: "Shipped",
+      Amount: "₹2,500",
+      Date: "Apr 17, 2024"
+    },
+    {
+      id: "#1020",
+      Customer: "Sarah Khan",
+      status: "Cancelled",
+      Amount: "₹1,100",
+      Date: "Apr 15, 2024"
     }
   ];
 
+  const getStatusStyle = (status: any) => {
+    switch (status) {
+      case "Delivered":
+        return "bg-green-100 text-green-600";
+      case "Pending":
+        return "bg-yellow-100 text-yellow-600";
+      case "Shipped":
+        return "bg-blue-100 text-blue-600";
+      case "Cancelled":
+        return "bg-red-100 text-red-600";
+      default:
+        return "";
+    }
+  };
+
   return (
-    <div className="bg-white mt-10 rounded-xl p-5 shadow-sm ">
+    <div className="bg-white mt-10 rounded-xl p-2 shadow-sm ">
       <div className="">
         <p className="font-bold text-xl">Recent Order</p>
       </div>
@@ -31,11 +64,29 @@ const SellerRecentOrder = () => {
             </tr>
           </thead>
 
-          <tbody>{}</tbody>
+          <tbody>
+            {Order.map((order, index) => (
+              <tr key={index} className="border-b hover:bg-gray-50">
+                <td className="py-3 font-medium">{order.id}</td>
+                <td className="py-3 font-medium">{order.Customer}</td>
+
+                <td className="py-3">
+                  <span
+                    className={`px-3 py-1 rounded-full text-sm font-medium ${getStatusStyle(order.status)} `}
+                  >
+                    {order.status}
+                  </span>
+                </td>
+
+                <td className="py-3">{order.Amount}</td>
+                <td className="py-3">{order.Date}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
-  );
+  );  
 };
 
 export default SellerRecentOrder;
