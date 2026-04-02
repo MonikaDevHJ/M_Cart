6e4d13836505f67868a2594923c510f64ad5e000
Monika H J <monikahj7@gmail.com>
2026-04-02 15:18:32 +0530

    done

--- app/seller/products/page.tsx
+++ app/seller/products/page.tsx
@@ -1,10 +1,36 @@
-
 const SellerProducts = () => {
   return (
-    <div>
-      No of Product
-    </div>
-  )
-}
+    <div className="">
+      <div className="">
+        <p className="font-bold text-3xl "> Products</p>
+      </div>
 
-export default SellerProducts
+       {/*  */}
+      <div className="mt-5">
+
+        <div className="flex justify-between">
+
+          <div className="">
+            <button>+ Add New</button>
+
+          </div>
+
+          <div className="">
+                  <p>Searhc Button</p>
+          </div>
+
+        </div>
+
+
+
+
+      </div>
+      {/* Tables */}
+      <div className="">
+
+      </div>
+    </div>
+  );
+};
+
+export default SellerProducts;
