705f7f5ce3a541805e53991a9fb86a38670f3c8a
Monika H J <monikahj7@gmail.com>
2026-04-03 18:01:29 +0530

    groccery Items and Filter is Making

--- app/(main)/grocery/page.tsx
+++ app/(main)/grocery/page.tsx
@@ -1,10 +1,18 @@
+import Filter from "@/app/component/grocery/Filter";
+import Items from "@/app/component/grocery/Items";
 
 const Grocery = () => {
   return (
-    <div className='items-center justify-center  text-center mt-10 '>
-      <p>Grocery Items UI is comming Soon</p>
-    </div>
-  )
-}
+    <div className="w-full grid grid-cols-5  gap-4 p-5 mt-5">
+      <div className="cols-span-2">
+        <Filter/>
+      </div>
 
-export default  Grocery
+      <div className="cols-span-3">
+        <Items />
+      </div>
+    </div>
+  );
+};
+
+export default Grocery;
