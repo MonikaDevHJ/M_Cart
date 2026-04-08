1d8cf3cd2963e2ff9107f582f3dd5aeb7e1de1df
Monika H J <monikahj7@gmail.com>
2026-04-08 14:30:54 +0530

    making product UI

--- app/seller/products/page.tsx
+++ app/seller/products/page.tsx
@@ -2,32 +2,36 @@ const SellerProducts = () => {
   return (
     <div className="">
       <div className="">
-        <p className="font-bold text-3xl "> Products</p>
+        <p className="font-bold text-3xl">Products</p>
       </div>
 
-       {/*  */}
+      {/*  */}
       <div className="mt-5">
-
         <div className="flex justify-between">
+          <button className="border rounded-lg bg-blue-600 p-2 text-white  font-medium hover:bg-blue-700">
+            + Add New
+          </button>
 
-          <div className="">
-            <button>+ Add New</button>
-
-          </div>
-
-          <div className="">
-                  <p>Searhc Button</p>
-          </div>
-
+          <input type="text" placeholder="Search..." className="border px-3 py-2 rounded-lg " />
         </div>
-
-
-
-
       </div>
-      {/* Tables */}
-      <div className="">
 
+      {/* Tables */}
+      <div className="mt-6 bg-white rounded-xl shadow p-4">
+        <table className="w-full text-left">
+          <thead className="">
+            <tr className="text-gray-600 border-b">
+              <th className="py-2">Image</th>
+              <th className="">Name</th>
+              <th className="">Stock</th>
+              <th className="">Status</th>
+              <th className="">Action</th>
+            </tr>
+          </thead>
+          <tbody className="">
+
+          </tbody>
+        </table>
       </div>
     </div>
   );
