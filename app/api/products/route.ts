a26d1f45a025db0f093b823adb6b3d7cecaf151e
Monika H J <monikahj7@gmail.com>
2026-04-15 16:45:44 +0530

    cloud things done

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import cloudinary from "@/lib/cloudinary";
 
 export async function POST(req: Request) {
   try {
-    const data = await req.formData(); // ✅ change here
+    const data = await req.formData();
 
     const name = data.get("name") as string;
     const description = data.get("description") as string;
@@ -11,6 +12,28 @@ export async function POST(req: Request) {
     const category = data.get("category") as string;
     const stock = data.get("stock") as string;
 
+    // ✅ Get image
+    const file = data.get("image") as File;
+
+    let imageUrl = "";
+
+    if (file) {
+      const bytes = await file.arrayBuffer();
+      const buffer = Buffer.from(bytes);
+
+      const upload = await new Promise((resolve, reject) => {
+        cloudinary.uploader
+          .upload_stream({ folder: "products" }, (error, result) => {
+            if (error) reject(error);
+            else resolve(result);
+          })
+          .end(buffer);
+      });
+
+      imageUrl = (upload as any).secure_url;
+    }
+
+    // ✅ Save in DB
     const product = await prisma.product.create({
       data: {
         name,
@@ -18,6 +41,7 @@ export async function POST(req: Request) {
         price: Number(price),
         category,
         stock: Number(stock),
+        image_url: imageUrl, // 🔥 important
       },
     });
 
