04452771c61349e3ec1c397111c7c22de55d1685
Monika H J <monikahj7@gmail.com>
2026-04-20 15:09:27 +0530

    done

--- app/seller/editproduct/[id]/page.tsx
+++ app/seller/editproduct/[id]/page.tsx
@@ -1,15 +1,45 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const EditProduct = () => {
-  const params = useParams();
+  const { id } = useParams();
 
-  console.log(params.id);
+  const [product, setProducts] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch(`/api/products/${id}`);
+      const data = await res.json();
+
+      setProducts(data);
+    };
+    if (id) fetchProducts();
+  }, [id]);
+
   return (
     <div className="p-5">
-      <h1>Edit Product Page</h1>
-      <p> ID : {params.id} </p>
+      <h1>Edit Your This Page</h1>
+
+      {!product ? (
+        <p>Loading..........</p>
+      ) : (
+        <div>
+          <p>
+            <b>Name:</b>
+            {product.name}
+          </p>
+          <p>
+            <b>Price : </b>
+            {product.price}
+          </p>
+          <p>
+            <b>Stock : </b>
+            {product.stock}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
