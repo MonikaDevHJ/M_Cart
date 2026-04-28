f350f6fc895421785095f2bcc25ad4d6d760441a
Monika H J <monikahj7@gmail.com>
2026-04-28 11:53:55 +0530

    iamge section in flipcart in Buyer

--- app/component/womens/Items.tsx
+++ app/component/womens/Items.tsx
@@ -1,13 +1,48 @@
-
 const Items = () => {
+  const Card = [
+    {
+      image: "Kurtha Image",
+      Name: "Kurtha",
+      Amout: "Red Color"
+    },
+    {
+      image: "Jeans Image ",
+      Name: "Kurtha",
+      Amout: "Red Color"
+    }
+  ];
+
   return (
     <div>
       <div className="">
-        <p className="font-extrabold text-2xl text-fuchsia-700">Pick You'r Drees</p>
+        <p className="font-extrabold text-2xl text-fuchsia-700">
+          Pick You'r Drees
+        </p>
+      </div>
+      <div className="p-5">
+        <div className="flex gap-10 mt-10 ">
+          {Card.map(
+            (item, index) => (
+              <div key={index} className="border p-4 ">
+                <div>
+                  <p className="">{item.image}</p>
+                </div>
+                <div className="">
 
+                  <p>{item.Amout}</p>
+                </div>
+
+                <div>
+                  <p>{item.Name}</p>  
+                </div>
+              </div>
+            ),
+            []
+          )}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Items
+export default Items;
