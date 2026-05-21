d5d907604ade4f4ca500cc581c5c4a6ab24a3bee
Monika H J <monikahj7@gmail.com>
2026-05-21 12:52:40 +0530

    cart Ui is Done

--- app/(main)/cart/page.tsx
+++ app/(main)/cart/page.tsx
@@ -4,7 +4,7 @@ import Amount from "../../component/cart/Amount"
 
 const page = () => {
   return (
-    <div className="p-5 bg-gray-200">
+    <div className="p-0.5 bg-gray-200">
 
         <div className="grid grid-cols-5 gap-5">
             <div className="col-span-4">
