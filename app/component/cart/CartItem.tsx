a7ea3065c36c9c0ec80a30a6f82ccccf477bbe30
Monika H J <monikahj7@gmail.com>
2026-07-07 14:18:04 +0530

    done

--- app/component/cart/CartItem.tsx
+++ app/component/cart/CartItem.tsx
@@ -73,6 +73,23 @@ const Item = () => {
     }
   };
 
+  // Delete Cart Item
+  const deleteCartItem = async (id: string) => {
+    try {
+      const res = await fetch(`/api/cart/${id}`, {
+        method: "DELETE"
+      });
+
+      const data = await res.json();
+
+      console.log(data);
+
+      await fetchCartItems();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   console.log(cartItems);
 
   return (
@@ -105,132 +122,140 @@ const Item = () => {
             </div>
           </div>
         </div>
-
         {/* CART ITEM */}
-        {cartItems.map((item: any) => (
-          <div
-            key={item.id}
-            className="bg-white shadow-lg border border-gray-200 rounded-3xl p-8 md:p-8 lg:p-8 mt-6"
-          >
-            <div className="grid grid-cols-1 lg:grid-cols-4 gap-15">
-              {/* LEFT SIDE */}
-              <div className="lg:col-span-1">
-                {/* IMAGE */}
-                <div className="bg-gray-100 rounded-3xl p-5 flex justify-center items-center">
-                  <Image
-                    src={item.product.productImage}
-                    alt={item.product.productName}
-                    width={290}
-                    height={290}
-                  />
+        {cartItems.map((item: any) => {
+          const totalPrice = item.product.productPrice * item.quantity;
+
+          return (
+            <div
+              key={item.id}
+              className="bg-white shadow-lg border border-gray-200 rounded-3xl p-8 md:p-8 lg:p-8 mt-6"
+            >
+              <div className="grid grid-cols-1 lg:grid-cols-4 gap-15">
+                {/* LEFT SIDE */}
+                <div className="lg:col-span-1">
+                  {/* IMAGE */}
+                  <div className="bg-gray-100 rounded-3xl p-5 flex justify-center items-center">
+                    <Image
+                      src={item.product.productImage}
+                      alt={item.product.productName}
+                      width={290}
+                      height={290}
+                    />
+                  </div>
+
+                  {/* QUANTITY */}
+                  <div className="mt-8">
+                    <p className="font-semibold text-gray-700 mb-3">Quantity</p>
+
+                    <div className="flex items-center gap-3">
+                      <button
+                        onClick={() => decreaseQuantity(item.id, item.quantity)}
+                        className="bg-gray-200 hover:bg-gray-300 p-3 rounded-xl transition"
+                      >
+                        <Minus size={18} />
+                      </button>
+
+                      <div className="border border-gray-300 px-6 py-2 rounded-xl font-semibold text-lg">
+                        {item.quantity}
+                      </div>
+
+                      <button
+                        onClick={() => increaseQuantity(item.id, item.quantity)}
+                        className="bg-gray-200 hover:bg-gray-300 p-3 rounded-xl transition"
+                      >
+                        <Plus size={18} />
+                      </button>
+                    </div>
+                  </div>
                 </div>
 
-                {/* QUANTITY */}
-                <div className="mt-8">
-                  <p className="font-semibold text-gray-700 mb-3">Quantity</p>
+                {/* RIGHT SIDE */}
+                <div className="lg:col-span-3 flex flex-col justify-between">
+                  {/* PRODUCT DETAILS */}
+                  <div className="ml-5">
+                    {/* TITLE */}
+                    <h1 className="text-xl lg:text-1xl md:text-3xl font-bold text-gray-800 leading-snug">
+                      {item.product.productName}
+                    </h1>
 
-                  <div className="flex items-center gap-3">
-                    <button
-                      onClick={() => decreaseQuantity(item.id, item.quantity)}
-                      className="bg-gray-200 hover:bg-gray-300 p-3 rounded-xl transition"
-                    >
-                      <Minus size={18} />
-                    </button>
+                    {/* SUBTITLE */}
+                    <p className="text-gray-500 ">
+                      Titanium Finish | A17 Pro Chip | Dynamic Island
+                    </p>
 
-                    <div className="border border-gray-300 px-6 py-2 rounded-xl font-semibold text-lg">
-                      {item.quantity}
+                    {/* RATINGS */}
+                    <div className="flex items-center gap-3 mt-2 flex-wrap">
+                      <div className="flex items-center bg-green-600 text-white px-3 py-1 rounded-full text-sm font-medium gap-1">
+                        4.2
+                        <Star size={10} fill="white" />
+                      </div>
+
+                      <p className="text-gray-500 text-sm">
+                        12,540 Ratings & 1,220 Reviews
+                      </p>
                     </div>
 
-                    <button
-                      onClick={() => increaseQuantity(item.id, item.quantity)}
-                      className="bg-gray-200 hover:bg-gray-300 p-3 rounded-xl transition"
-                    >
-                      <Plus size={18} />
-                    </button>
-                  </div>
-                </div>
-              </div>
+                    {/* PRICE */}
+                    <div className="flex flex-wrap items-center gap-4 mt-3">
+                      <h2 className="text-2xl lg:text-xl md:text-4xl font-bold text-gray-900">
+                        ₹ {totalPrice}
+                      </h2>
 
-              {/* RIGHT SIDE */}
-              <div className="lg:col-span-3 flex flex-col justify-between">
-                {/* PRODUCT DETAILS */}
-                <div className="ml-5">
-                  {/* TITLE */}
-                  <h1 className="text-xl lg:text-1xl md:text-3xl font-bold text-gray-800 leading-snug">
-                    {item.product.productName}
-                  </h1>
+                      <p className="line-through text-gray-400 text-lg">
+                        ₹1,39,999
+                      </p>
 
-                  {/* SUBTITLE */}
-                  <p className="text-gray-500 ">
-                    Titanium Finish | A17 Pro Chip | Dynamic Island
-                  </p>
-
-                  {/* RATINGS */}
-                  <div className="flex items-center gap-3 mt-2 flex-wrap">
-                    <div className="flex items-center bg-green-600 text-white px-3 py-1 rounded-full text-sm font-medium gap-1">
-                      4.2
-                      <Star size={10} fill="white" />
+                      <p className="text-green-600 font-bold text-lg">
+                        10% OFF
+                      </p>
                     </div>
 
-                    <p className="text-gray-500 text-sm">
-                      12,540 Ratings & 1,220 Reviews
-                    </p>
+                    <div>
+                      <p>Size: {item.product.productSize}</p>{" "}
+                    </div>
+
+                    {/* DELIVERY */}
+                    <div className="mt-3">
+                      <p className="text-gray-700 text-lg">
+                        Delivery by{" "}
+                        <span className="font-bold text-green-600">Monday</span>
+                      </p>
+
+                      <p className="text-sm text-gray-500 mt-1">
+                        Free Delivery Available
+                      </p>
+                    </div>
                   </div>
 
-                  {/* PRICE */}
-                  <div className="flex flex-wrap items-center gap-4 mt-3">
-                    <h2 className="text-2xl lg:text-xl md:text-4xl font-bold text-gray-900">
-                      ₹ {item.product.productPrice}
-                    </h2>
+                  {/* ACTION BUTTONS */}
+                  <div className="flex flex-wrap gap-4 mt-5">
+                    {/* SAVE */}
+                    <button className="flex items-center gap-2 bg-gray-300 hover:bg-gray-200 px-5 py-1 rounded-2xl font-semibold transition">
+                      <Heart size={15} />
+                      Save For Later
+                    </button>
 
-                    <p className="line-through text-gray-400 text-lg">
-                      ₹1,39,999
-                    </p>
+                    {/* REMOVE */}
+                    <button
+                      onClick={() => deleteCartItem(item.id)}
+                      className="flex items-center gap-2 bg-red-100 text-red-600 hover:bg-red-200 px-5 py-2 rounded-2xl font-semibold transition"
+                    >
+                      <Trash2 size={15} />
+                      Remove
+                    </button>
 
-                    <p className="text-green-600 font-bold text-lg">10% OFF</p>
+                    {/* BUY NOW */}
+                    <button className="flex items-center gap-2 bg-fuchsia-600 hover:bg-fuchsia-700 text-white px-6 py-3 rounded-2xl font-semibold shadow-lg transition">
+                      <ShoppingBag size={15} />
+                      Buy Now
+                    </button>
                   </div>
-
-                  <div>
-                    <p>Size: {item.product.productSize}</p>{" "}
-                  </div>
-
-                  {/* DELIVERY */}
-                  <div className="mt-3">
-                    <p className="text-gray-700 text-lg">
-                      Delivery by{" "}
-                      <span className="font-bold text-green-600">Monday</span>
-                    </p>
-
-                    <p className="text-sm text-gray-500 mt-1">
-                      Free Delivery Available
-                    </p>
-                  </div>
-                </div>
-
-                {/* ACTION BUTTONS */}
-                <div className="flex flex-wrap gap-4 mt-5">
-                  {/* SAVE */}
-                  <button className="flex items-center gap-2 bg-gray-300 hover:bg-gray-200 px-5 py-1 rounded-2xl font-semibold transition">
-                    <Heart size={15} />
-                    Save For Later
-                  </button>
-
-                  {/* REMOVE */}
-                  <button className="flex items-center gap-2 bg-red-100 text-red-600 hover:bg-red-200 px-5 py-2 rounded-2xl font-semibold transition">
-                    <Trash2 size={15} />
-                    Remove
-                  </button>
-
-                  {/* BUY NOW */}
-                  <button className="flex items-center gap-2 bg-fuchsia-600 hover:bg-fuchsia-700 text-white px-6 py-3 rounded-2xl font-semibold shadow-lg transition">
-                    <ShoppingBag size={15} />
-                    Buy Now
-                  </button>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}{" "}
       </div>
     </div>
   );
