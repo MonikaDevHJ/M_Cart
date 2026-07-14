a487ca106373419fdc2fbde0392c573fac16bf86
Monika H J <monikahj7@gmail.com>
2026-07-14 21:21:19 +0530

    making Buyer Dashboard

---
+++ app/buyer/layout.tsx
@@ -0,0 +1,25 @@
+import BuyerSideBar from "../component/buyer/dashboard/BuyerSidebar";
+
+export default function SellerLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="lg:p-0.5 sm:p-5 ">
+      <div className="flex flex-col md:flex-row gap-1">
+        {/* Sidebar */}
+        <div className="md:w-1/4 lg:w-1/5">
+          <BuyerSideBar />
+        </div>
+
+        {/* Main Content */}
+        <div className="flex-1  mt-4 md:mt-0 ">
+        
+
+          <div className="p-5">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
