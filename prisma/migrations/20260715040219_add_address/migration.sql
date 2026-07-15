94cab11e4101c6b7f7c1da750d523f18b81b32aa
Monika H J <monikahj7@gmail.com>
2026-07-15 11:24:04 +0530

    done

---
+++ prisma/migrations/20260715040219_add_address/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "houseNo" TEXT NOT NULL,
+    "area" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "pincode" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
