dc82fe9cbed7c7e390b1e7db9dbc082bed777c65
Monika H J <monikahj7@gmail.com>
2026-07-27 18:50:24 +0530

    making recent order in Buyer Dashboard

--- app/component/buyer/dashboard/RecommendedProducts.tsx
+++ app/component/buyer/dashboard/RecommendedProducts.tsx
@@ -1,10 +1,25 @@
+import Image from "next/image";
+import { Heart, ChevronRight } from "lucide-react";
 
 const RecommendedProducts = () => {
   return (
-    <div>
-      <p>Recomeneded Jobs</p>
-    </div>
-  )
-}
+    <div className="bg-white rounded-2xl border border-gray-200 shadow-sm p-4 sm:p-5">
+      {/* Header */}
+      <div className="flex items-center justify-between mb-5">
+        <div>
+          <p className="text-lg font-bold text-gray-800">Recommended For You</p>
+          <p className="text-xs">Based on Your recent activity</p>
+        </div>
 
-export default RecommendedProducts
+        <div>
+          <button className="flex items-center gap-1 text-sm font-semibold text-purple-600 hover:text-purple-700 transition">
+            View All
+            <ChevronRight size={16} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedProducts;
