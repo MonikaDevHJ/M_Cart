6e5bcf6c0fa2c6139b21b21f18618b3919e2aa4c
Monika H J <monikahj7@gmail.com>
2026-09-08 17:18:01 +0530

    profile Section making

--- app/(main)/buyerprofile/page.tsx
+++ app/(main)/buyerprofile/page.tsx
@@ -1,4 +1,27 @@
 const BuyerProfile = () => {
+  const cards = [
+    {
+      icon: "MyOrder",
+      name: "My_Order",
+      Details: "View your Order History"
+    },
+    {
+      icon: "WishList",
+      name: "WishList",
+      Details: "Your Saved Items"
+    },
+    {
+      icon : "Addresses",
+      name : "you Location",
+      Details : "Manage delivery address"
+    },
+     {
+      icon : "cart",
+      name : "you Cart",
+      Details : "Your Items"
+    }
+  ];
+
   return (
     <div className="p-10 ">
       {/* text */}
@@ -14,16 +37,47 @@ const BuyerProfile = () => {
       </div>
 
       {/* Iamge and Information Section */}
-      <div className="mt-10 p-5">
-        <div className="flex">
-          {/* Image Sextion */}
+      <div className="mt-10 p-5 flex">
+        <div className="flex gap-10">
+          {/* Image Section */}
           <div className="">Image</div>
 
-         {/* information Section  */}
+          {/* information Section  */}
           <div className="">
-            
+            <div className="">
+              <input type="text" name="" id="" placeholder="Enter Your Name " />
+            </div>
+
+            <div className="">
+              <input
+                type="email"
+                name=""
+                id=""
+                placeholder="Enter Your Email "
+              />
+            </div>
+
+            <div className="">
+              <input
+                type="number"
+                name=""
+                id=""
+                placeholder="Enter Your Phone Number "
+              />
+            </div>
           </div>
         </div>
+        {/* Button */}
+        <div className="">
+          <button className="border border-fuchsia-700 rounded-2xl p-2">
+            Edit
+          </button>
+        </div>
+      </div>
+
+      {/* card Section */}
+      <div className="">
+        
       </div>
     </div>
   );
